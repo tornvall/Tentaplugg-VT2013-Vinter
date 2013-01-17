1396a03f03b9802ac24f9ff508215e976ef5bbb0
tornvall <f.tornvall@gmail.com>
2013-01-17 17:50:15 +0100

    The beginning of a snowplow

---
+++ CG20120829q6SnowplowRoad/CG20120829q6SnowplowRoad/CG20120829q6SnowplowRoad/Snowplow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace CG20120829q6SnowplowRoad {
+    public class Snowplow {
+        private Model _model;
+        private Vector3 _position;
+
+        public Snowplow(Model model, Vector3 position) {
+            _model = model;
+            _position = position;
+        }
+    }
+}
