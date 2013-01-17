df3250ca586bf231e5b0c83d7641e3f5a0a5fdfa
Johan Dahlberg <info@johandahlberg.com>
2013-01-17 18:17:41 +0100

    Snowplow works

--- CG20120829q6SnowplowRoad/CG20120829q6SnowplowRoad/CG20120829q6SnowplowRoad/Snowplow.cs
+++ CG20120829q6SnowplowRoad/CG20120829q6SnowplowRoad/CG20120829q6SnowplowRoad/Snowplow.cs
@@ -30,6 +30,9 @@ namespace CG20120829q6SnowplowRoad {
 
         public void Draw()
         {
+            // Set the position of the camera in world space, for our view matrix.
+            Vector3 cameraPosition = new Vector3(0.0f, 50.0f, 5000.0f);
+
             // Copy any parent transforms.
             Matrix[] transforms = new Matrix[_model.Bones.Count];
             _model.CopyAbsoluteBoneTransformsTo(transforms);
@@ -45,11 +48,11 @@ namespace CG20120829q6SnowplowRoad {
                     effect.World = transforms[mesh.ParentBone.Index] *
                         Matrix.CreateRotationY(_rotation)
                         * Matrix.CreateTranslation(_position);
-                    /*effect.View = Matrix.CreateLookAt(cameraPosition,
+                    effect.View = Matrix.CreateLookAt(cameraPosition,
                         Vector3.Zero, Vector3.Up);
                     effect.Projection = Matrix.CreatePerspectiveFieldOfView(
                         MathHelper.ToRadians(45.0f), device.Viewport.AspectRatio,
-                        1.0f, 10000.0f);*/
+                        1.0f, 10000.0f);
                 }
                 // Draw the mesh, using the effects set above.
                 mesh.Draw();
